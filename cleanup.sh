a956c5188967a9e3b96d06dcc33867e54659981b
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-02-11 21:36:13 +0300

    some cleanup script fixes
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -e
 
+tag=$1
+
 for m in $(find ./ -name 'go.mod'); do
   d=$(dirname $m);
   pushd $d;
-  /bin/bash -c 'go get github.com/micro/cli/v2@master && go mod tidy' &
+  PKGS=$(grep github.com/micro/go-plugins go.mod | grep -v module | tr -s '\n' ' ' | awk '{print $1}')
+  for PKG in $PKGS; do
+    /bin/bash -c "go get $PKG/v2@$tag && go mod tidy" &
+  done
   popd;
 done

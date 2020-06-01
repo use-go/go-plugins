396b2eaadecd4f2fcbc8778173b50ed240e585f2
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-06-02 01:02:28 +0300

    fix
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- cleanup.sh
+++ cleanup.sh
@@ -5,8 +5,8 @@ tag=$1
 for m in $(find ./ -name 'go.mod'); do
   d=$(dirname $m);
   pushd $d;
-  grep github.com/micro/go-micro/v2 go.mod && go get github.com/micro/go-micro/v2@$tag
-  grep github.com/micro/micro/v2 go.mod && go get github.com/micro/micro/v2@$tag
+  grep -q github.com/micro/go-micro/v2 go.mod && go get github.com/micro/go-micro/v2@$tag
+  grep -q github.com/micro/micro/v2 go.mod && go get github.com/micro/micro/v2@$tag
   go mod tidy
   popd;
 done
